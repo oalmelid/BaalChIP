7ab5fca5f3d9d1928b66455c403ddecbfc876ea1
Øyvind Almelid <oyvind.almelid@igmm.ed.ac.uk>
2020-12-14 14:56:25 +0000

    clean up

--- R/Bayes_report.R
+++ R/Bayes_report.R
@@ -2,7 +2,7 @@
 #Ines de Santiago, Wei Liu, Ke Yuan, Florian Markowetz
 
 
-Bayesian_report <- function(iter_matrix,conf_level,threshold_lower,threshold_upper,burnin,maxlag,SNP_check,SNP){
+Bayesian_report <- function(SNP_id, iter_matrix,conf_level,threshold_lower,threshold_upper,burnin,maxlag,SNP_check){
     #suppressPackageStartupMessages(require('coda'))
     traces <- iter_matrix
 
@@ -15,17 +15,16 @@ Bayesian_report <- function(iter_matrix,conf_level,threshold_lower,threshold_upp
     }
 
     mcmc_traces = mcmc_converter(traces, burnin)
-    conf_itval <- matrix(NA,1,5)
-
-    conf_itval[SNP,1:2] = HPDinterval(mcmc_traces[[SNP]], prob = conf_level)
-    cconf_itval[SNP,3] = (threshold_upper <= conf_itval[SNP,1])
-    cconf_itval[SNP,4] = (threshold_lower >= conf_itval[SNP,2])
-    stat_summaries <- summary(mcmc_traces[[SNP]])[["statistics"]]
-    conf_itval[SNP,5] = stat_summaries["SD"]
     
-    conf_itval <- data.frame(SNP, conf_itval, stringsAsFactors=FALSE)
-
-    ##################
-    colnames(conf_itval) <- c("ID","Bayes_lower", "Bayes_upper","Bayes_sig_A", "Bayes_sig_B", "Bayes_SD")
-    return(conf_itval)
+    conf_itval <- matrix(NA,1,5)
+    conf_itval[SNP_id,1:2] = HPDinterval(mcmc_traces[[SNP_id]], prob = conf_level)
+    conf_itval[SNP_id,3] = (threshold_upper <= conf_itval[SNP_id,1])
+    conf_itval[SNP_id,4] = (threshold_lower >= conf_itval[SNP_id,2])
+    stat_summaries <- summary(mcmc_traces[[SNP_id]])[["statistics"]]
+    conf_itval[SNP_id,5] = stat_summaries["SD"]
+    
+    conf_itval_df <- data.frame(c(SNP_id), conf_itval, stringsAsFactors=FALSE)
+    colnames(conf_itval_df) <- c("ID","Bayes_lower", "Bayes_upper","Bayes_sig_A", "Bayes_sig_B", "Bayes_SD")
+    
+    return(conf_itval_df)
 }
